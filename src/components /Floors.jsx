5f9408d114f5dcd3cdd12299ab6df005d1fda7ba
Mehul Kiran Chaudhari <chaudharimehul2001@gmail.com>
2021-11-09 06:35:53 +0530

    added: floor component

---
+++ src/components /Floors.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Floor.css'
+
+const Floors = () => {
+  return (
+    <div className="floor">
+      <h4>Floor One</h4>
+    </div>
+  );
+};
+
+export default Floors;
