32c23e5fde270586276096d8d449acc4cdead0a2
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2021-11-28 10:13:10 +0530

    added: footer (#8)

---
+++ src/components /Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+           Contents of this are website are deployed from this: <a href='https://github.com/MehulKChaudhari/lift-simulation-react'>repo</a>
+        </footer>
+    )
+}
+
+export default Footer
